636201ca80940835c85961fc12a15b224671b6b5
Roger Day <notsofeo@gmail.com>
2015-04-06 17:05:55 -0400

    Better coordination among the 4 NNT inputs.

--- nntbiomarker/inst/shinyElicit/server.R
+++ nntbiomarker/inst/shinyElicit/server.R
@@ -7,18 +7,37 @@ shinyServer(function(input, output, session) {
   #source("conveniences.R", local=TRUE)
   source("../debugTools.R", local=TRUE)
   source("plotDiscomfort.R", local=TRUE)
+  NNTgap = 1
   observe( {
-    cat("updating numerical input\n")
     updateNumericInput(session, inputId="NNTpos",
-                       max = input$NNTlower)
+                       value=min(input$NNTpos,
+                                 input$NNTlower-NNTgap),
+                       max = input$NNTlower-NNTgap)
+  })
+  observe( {
+    updateNumericInput(session, inputId="NNTlower",
+                       value=max(input$NNTlower,
+                                 input$NNTpos+NNTgap),
+                       min = input$NNTpos+NNTgap)
+  })
+  observe( {
     updateNumericInput(session, inputId="NNTneg",
-                       min = input$NNTupper)
+                       value=max(input$NNTneg,
+                                 input$NNTupper+NNTgap),
+                       min = input$NNTupper+NNTgap)
+  })
+  observe( {
+    updateNumericInput(session, inputId="NNTupper",
+                       value=min(input$NNTupper,
+                                 input$NNTneg-NNTgap),
+                       max = input$NNTneg-NNTgap)
   })
   output$plotDiscomfort = renderPlot({
   plotDiscomfort(  NNTlower = input$NNTlower,
-                   NNTupper = input$NNTupper)
-}
-  )
+                   NNTupper = input$NNTupper,
+                   NNTpos = input$NNTpos,
+                   NNTneg = input$NNTneg)
+  })
 })
 
 
