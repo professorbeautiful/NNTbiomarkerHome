554c904c6e8b9325b4f971e8d703052284e0de7e
Roger Day <notsofeo@gmail.com>
2015-04-10 12:57:50 -0400

    Moving up to parent folder, see if that helps with deployApp().

--- nntbiomarker/inst/NNTbiomarker.R
+++
@@ -1,8 +0,0 @@
-source("R/sesp-pv-NNT.R", local=TRUE)
-
-source("R/achievable.se.sp.R", local=TRUE)
-
-source("R/binom.confint.R", local=TRUE)
-
-#source("R/NNTconfidenceIntervals.R", local=TRUE)
-
--- nntbiomarker/inst/shinyContraBayes/ui.R
+++
@@ -1,7 +0,0 @@
-shinyUI(basicPage(
-  h1("Achievable sensitivity and specificity"),
-  numericInput("prevalence", label = "prevalence",
-               value=0.5, min = 0, max=1, step = 0.05),
-  plotOutput("myplot", hoverId="myplot_hover"),
-  tableOutput("hoverinfo")
-))
