1eafa4260e626a24fa618d31f3e4bc1804b3efb5
Roger Day <notsofeo@gmail.com>
2015-04-15 23:01:49 -0400

    Adding hardlink copies of contraBayesPlot and debugTools to shinyElicit

---
+++ inst/shinyElicit/contraBayesPlot.R
@@ -0,0 +1,41 @@
+output$contraBayesPlot <- renderPlot({
+  achievable.se.sp(the.prev = input$prevalence)
+})
+
+# rValues$selectedNNTPosNeg =
+
+output$selectedNNTPosNeg = renderTable({
+  hD = hoverData()
+  if(is.null(hD)) NULL
+  else
+    xtable(digits=3, hD)
+})
+
+hoverData = reactive({
+  #catn("hoverData")
+  # TODO: currently assumes  axes = "pv",
+  # as opposed to NNTpos, NNTneg.
+  ppv = input$contraBayesPlot_hover$x
+  npv = input$contraBayesPlot_hover$y
+  the.prev = input$prevalence
+  nnt = NNT.from.pv(ppv = ppv, npv=npv)
+  sesp = sesp.from.pv(ppv = ppv, npv=npv, prev=the.prev)
+  result = try(silent = TRUE, t(data.frame(
+    sensitivity=sesp["se"], specificity=sesp["sp"],
+    PPV=ppv,
+    NPV=npv,
+    NNTpos=nnt[1],
+    NNTneg=nnt[2]
+  )))
+  if(class(result) == "try-error")
+    return (NULL)
+  names(result) = "value"
+  result
+})
+#catn("is.reactive(hoverData)= ", is.reactive(hoverData))
+
+observe({
+    catn("hoverinfo ", capture.output(input$contraBayesPlot_hover))
+    if(!is.null(input$contraBayesPlot_hover))
+       rValues$hoverinfo <- hoverData()
+})
