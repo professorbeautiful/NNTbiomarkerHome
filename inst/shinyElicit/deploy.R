bf89575508f744b521dcefa651ea35fba5908360
Roger Day <notsofeo@gmail.com>
2015-04-17 19:45:35 -0400

    Added runDeployed()

--- inst/shinyElicit/deploy.R
+++ inst/shinyElicit/deploy.R
@@ -1,3 +1,5 @@
+# shinyapps::setAccountInfo(name='trials', token='0B4BEE2FB18CEA0BCFD76843D64453A5', secret='z30BF4mYXM3llWXFsbjF7I826F8DtVqaXUN3UyjY')
+
 deploy = function(app="shinyElicit"){
   devtools::install_github("professorbeautiful/NNTbiomarkerHome")
   setwd("~/Dropbox/NNTbiomarkerHome/inst/" %&% app)
@@ -5,3 +7,8 @@ deploy = function(app="shinyElicit"){
   deployApp()
   setwd("../..")
 }
+
+runDeployed = function(app="shinyElicit"){
+  system("open https://trials.shinyapps.io/" %&% app)
+}
+
