125af71be0ad18b39ab5c8727d94f2c45c080f4c
Roger Day <notsofeo@gmail.com>
2015-04-18 00:20:45 -0400

    Adding comments on preventing a deploy failure.

--- inst/shinyElicit/deploy.R
+++ inst/shinyElicit/deploy.R
@@ -1,5 +1,14 @@
 # shinyapps::setAccountInfo(name='trials', token='0B4BEE2FB18CEA0BCFD76843D64453A5', secret='z30BF4mYXM3llWXFsbjF7I826F8DtVqaXUN3UyjY')
 
+# NOTE: I had tried to run rstan, and followed directions
+# for soft links as in "NOTES on rstan for monte carlo markov chain.rtf"
+# That messed up the deploy:
+#   *** arch - R
+# ERROR: sub-architecture 'R' is not installed
+# *** arch - x86_64
+# ERROR: sub-architecture 'x86_64' is not installed
+# Solution remove the soft links especially the third one.
+
 deploy = function(app="shinyElicit"){
   devtools::install_github("professorbeautiful/NNTbiomarkerHome")
   setwd("~/Dropbox/NNTbiomarkerHome/inst/" %&% app)
