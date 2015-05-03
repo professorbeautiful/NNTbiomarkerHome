a2b473e03d170f1c0c63da4aadb34f6388c8d44e
Roger Day <notsofeo@gmail.com>
2015-05-03 12:12:33 -0400

    Added reactives to help shinyContraBayes work.

--- inst/shinyContraBayes/server.R
+++ inst/shinyContraBayes/server.R
@@ -5,5 +5,8 @@ appName = "shinyContraBayes"
 
 shinyServer(function(input, output, session) {
   rValues = reactiveValues()
+  PPVderived = reactive({1/input$NNTpos})
+  NPVderived = reactive({1 - 1/input$NNTneg})
+
   source("contraBayesPlot.R", local=T)
 })
