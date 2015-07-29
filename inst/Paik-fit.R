8245c49ed03b21b0faa16e50931852d8e034131d
lilyokc <lilyokc2012@gmail.com>
2015-07-29 16:30:37 -0400

    move code to AEplot

--- inst/Paik-fit.R
+++ inst/Paik-fit.R
@@ -343,31 +343,3 @@ with(tenYearDFS_long %>% subset(., group=="TAM"), {
 })
 
 
-#### Adverse event NNT bars ####
-#### For CMFT, from table 5 of fisher1997.
-
-boxcolors = colorRampPalette(c("lightgrey", "red"))(6)
-par(mai=c(0,0,1,0))
-par()
-for(RSforAEplot in c(OncotypeRScutoffs, TailorXRScutoffs)) {
-  aeProb = c(2.9,15,57,20,5,0.1)
-  boxwidths = c(1, (nnt[RSforAEplot] - 1) * aeProb / 100)
-  symbols(x=rep(0, 7), y=7:1, inches=F,
-          xlim=c(-ceiling(max(boxwidths)), ceiling(max(boxwidths))) * 0.75,
-          rectangles = cbind(boxwidths, 1), bg = c("green", boxcolors) ,
-          axes=F, xlab="", ylab="")
-  "%except%" <-  function (vector, condition) vector[match(vector, condition, 0) == 0]
-  verticalsX = lapply(boxwidths[-1], function(bw)
-    if(bw <= 1)  numeric(0)  else  -floor(bw/2):floor(bw/2)
-  )
-  verticalsY = rep(6:1, times=sapply(verticalsX, length))
-  segments(x0= unlist(verticalsX),
-           y0 = verticalsY - 1/2, y1 = verticalsY + 1/2
-  )
-  text(x = boxwidths/2, y=7:1,
-       c("benefitted", "no AE", "mild", "moderate", "severe", "life-threatening", "died"),
-       pos=4 , xpd=NA)
-  text(x = - boxwidths/2, y=7:1, round(boxwidths, 1),
-       pos=2 )
-  title(paste0("RS = ", RSforAEplot, "  NNT = ", round(nnt[RSforAEplot])))
-}
