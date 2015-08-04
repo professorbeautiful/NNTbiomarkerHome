5936368f531a9babad77235bcf00fcd2a4828d6e
Roger Day <notsofeo@gmail.com>
2015-08-04 01:24:52 -0400

    Add the makeTitle=TRUE arg to AEplot call in server.

--- inst/shinyAE/server.R
+++ inst/shinyAE/server.R
@@ -3,54 +3,54 @@ library(shiny)
 # options(shiny.error=recover)
 
 shinyServer(function(input, output, session) {
-  
+
   thisSession <<- session
-  
+
   source("skislope.R", local=TRUE)
   source("AEplot.R", local=TRUE)
-  
+
   rV = reactiveValues(RSclicked = 30)
-  
+
   observe({
     input$ODXlow
     updateNumericInput(inputId = "RSchosen",
-                       session = thisSession, 
+                       session = thisSession,
                        value = OncotypeRScutoffs[1])
   })
   observe({
     input$ODXhigh
     updateNumericInput(inputId = "RSchosen",
-                       session = thisSession, 
+                       session = thisSession,
                        value = OncotypeRScutoffs[2])
   })
   observe({
     input$TailorX_low
     updateNumericInput(inputId = "RSchosen",
-                       session = thisSession, 
+                       session = thisSession,
                        value = TailorXRScutoffs[1])
   })
   observe({
     input$TailorX_high
     updateNumericInput(inputId = "RSchosen",
-                       session = thisSession, 
+                       session = thisSession,
                        value = TailorXRScutoffs[2])
   })
-  
+
   output$skislope = renderPlot({
 #     if(is.null(input$skislope_click))
 #       RSarg = 49
 #     else RSarg = input$skislope_click$x
 #     RSarg = round(max(RSarg, 5))
-#     cat("============  Setting rV$RSclicked: ", rV$RSclicked, 
+#     cat("============  Setting rV$RSclicked: ", rV$RSclicked,
 #         " ======\n")
 #     isolate({
 #       rV$RSclicked = RSarg
 #     })
     skisloplot(input$RSchosen, input$ytop)
   })
-  
+
   output$AEstack = renderPlot({
-    AEplot(input$RSchosen)
+    AEplot(input$RSchosen, makeTitle=TRUE)
   })
 
 #   observe({
