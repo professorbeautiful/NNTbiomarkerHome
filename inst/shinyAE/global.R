245854e08bc7c7d85d7d493c52b90ed33263f02f
Roger Day <notsofeo@gmail.com>
2015-08-04 00:35:54 -0400

    Added NOTE: no "inst" when running from package home.

--- inst/shinyAE/global.R
+++ inst/shinyAE/global.R
@@ -1,4 +1,5 @@
 PaikFitFile = system.file(package="NNTbiomarker",  "Paik-fit.R")
+### NOTE: no "inst" when running from package home.
 cat("global.R: PaikFitFile is ", PaikFitFile, "\n")
 if( ! identical(ls(pattern="Paik_nnt", pos=1), 1) )
   source(PaikFitFile, local=TRUE)
